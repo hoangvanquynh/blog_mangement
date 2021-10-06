b15641738206004213bc70e2ce17e1a7a76c5cdd
quynhhoang <hoangvanquynhcda1995@gmail.com>
2021-10-06 22:51:09 +0700

    rename entity

--- BlogManagement/src/main/java/com/hvq/blog/entity/Category.java
+++ BlogManagement/src/main/java/com/hvq/blog/entity/Category.java
@@ -8,27 +8,29 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
-public class Categories{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long categoryId;
+@Table(name = "category")
+public class Category extends BaseEntity{
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long categoryId;
 	private String categoryName;
 	private String slug;
 	private String description;
-	private Date createAt;
-	private Date updateAt;
+//	private Date createAt;
+//	private Date updateAt;
 	
 	@ManyToMany(mappedBy = "categories")
 	private Set<Post> posts;
 	
-	public Long getCategoryId() {
-		return categoryId;
-	}
-	public void setCategoryId(Long categoryId) {
-		this.categoryId = categoryId;
-	}
+//	public Long getCategoryId() {
+//		return categoryId;
+//	}
+//	public void setCategoryId(Long categoryId) {
+//		this.categoryId = categoryId;
+//	}
 	public String getCategoryName() {
 		return categoryName;
 	}
@@ -53,17 +55,17 @@ public class Categories{
 	public void setPosts(Set<Post> posts) {
 		this.posts = posts;
 	}
-	public Date getCreateAt() {
-		return createAt;
-	}
-	public void setCreateAt(Date createAt) {
-		this.createAt = createAt;
-	}
-	public Date getUpdateAt() {
-		return updateAt;
-	}
-	public void setUpdateAt(Date updateAt) {
-		this.updateAt = updateAt;
-	}
-	
+//	public Date getCreateAt() {
+//		return createAt;
+//	}
+//	public void setCreateAt(Date createAt) {
+//		this.createAt = createAt;
+//	}
+//	public Date getUpdateAt() {
+//		return updateAt;
+//	}
+//	public void setUpdateAt(Date updateAt) {
+//		this.updateAt = updateAt;
+//	}
+//	
 }
